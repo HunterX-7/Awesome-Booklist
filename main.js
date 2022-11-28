a26df17e9bffeae9128d4bfc23742d7cbf81eb59
HunterX-7 <fraquin7@gmail.com>
2022-11-28 16:08:11 -0500

    Create an array to store the data and try to add logic to store the values in the localStorage

--- main.js
+++ main.js
@@ -1,26 +1,81 @@
 var books = [
     {
-        name: 'The little prince',
+        title: 'The little prince',
         author: 'Antoine de Saint-Exupéry' 
     },
     {
-        name: 'The figth club',
+        title: 'The figth club',
         author: 'Chuck Palahniuk' 
     }
 ]
 
-
 const book =  document.getElementById('book');
+let t = document.getElementById('title').value;
+let a = document.getElementById('author').value;
+let t1 = JSON.stringify(t);
+let a1 = JSON.stringify(a);
 
-for(let i = 0; i < books.length; i++){
-    let bookData = `
-    <div id="book${i}">
-        <h2>${books[i].name}</h2>
-        <p>${books[i].author}</p>
-        <button>x</button>
-    </div>
-    <hr>
-    `;
-    book.innerHTML += bookData;
-}
+console.log(t)
+console.log(a)
+console.log(t1)
+console.log(a1)
 
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+// document.addEventListener('change', () => {
+//     const title = document.getElementById('title');
+//     const author = document.getElementById('author');
+
+//     const data = {
+//         title: title.value,
+//         author: author.value,
+//     }
+
+//     const dataStr = JSON.stringify(data);
+
+//     localStorage.setItem(data, dataStr);
+// })
+
+// document.addEventListener('DOMContentLoaded', ()=> {
+//     const dataUnstr = JSON.parse(localStorage.getItem('data'));
+// })
+
+// function getBooks() {
+//     if(localStorage.getItem('books') === null){
+//         books = [];
+//     }
+//     else {
+//         books = JSON.parse(localStorage.getItem('books'))
+//     }
+// }
+
+// function storeBooks() {
+//     const books = 
+// }
+
+document.querySelector('#form').addEventListener('submit', () =>{
+    const newBook = {title: t1, author: a1};
+    books.push(newBook);
+})
+
+// function addBook() {
+//     books.push({title: t1, author: a1});
+//     for(let i = 0; i < books.length; i++){
+//         let bookData = `
+//         <div id="book${i}">
+//             <h2>${books[i].title}</h2>
+//             <p>${books[i].author}</p>
+//             <button>x</button>
+//         </div>
+//         <hr>
+//         `;  
+//         book.innerHTML = bookData;
+//     }
+// }
+
+
+
+
+console.log(books);
